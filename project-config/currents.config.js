61dc93f18c91789431096635074de54b90593b2d
Adam Stegienko <107151098+adam-stegienko@users.noreply.github.com>
2023-12-01 17:41:34 +0100

    Update currents.config.js

--- project-config/currents.config.js
+++ project-config/currents.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    projectId: process.env.PROJECT_ID,
-    recordKey: process.env.RECORD_KEY,
-    cloudServiceUrl: process.env.CLOUD_SERVICE_URL,
+    projectId: '<project-id>',
+    recordKey: '<record-key>',
+    cloudServiceUrl: '<director-service-fqdn>',
 };
