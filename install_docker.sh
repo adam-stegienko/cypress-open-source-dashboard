da267c43cf926ef61bb3597217ff8e823902e076
Adam Stegienko <adam.stegienko1@gmail.com>
2023-12-01 18:19:49 +0100

    docker install script updated

--- install_docker.sh
+++ install_docker.sh
@@ -11,12 +11,14 @@ agent_user=$(whoami)
 # Install docker from script
 docker version || curl -fsSL https://get.docker.com | bash -
 
-# add docker group to current user and activate it
-groups | grep docker || sudo usermod -aG docker $agent_user && newgrp docker
+# add docker group to current user
+if ! groups | grep docker > /dev/null; then
+    sudo usermod -aG docker $agent_user
+    echo "Added $agent_user to docker group. Please log out and log back in to apply changes."
+fi
 
 # Install docker-compose
-docker-compose version || sudo apt install docker-compose -y
+docker-compose version > /dev/null 2>&1 || sudo apt install docker-compose -y
 
 # Test docker installation and correct user permissions
 docker ps || echo "Insufficient permissions to run docker. Please log out and log back in."
-
